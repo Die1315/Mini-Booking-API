61e60d5fd1288ef54ad2c94d3002c2c12a560c1e
Diego Guauta <diego.g0912@hotmail.es>
2025-08-01 21:01:33 -0500

    Cleans up imports
    Removes unused imports from bookings/views.py and futurelab/urls.py, improving code readability and maintainability.

--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
 import requests
-
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
